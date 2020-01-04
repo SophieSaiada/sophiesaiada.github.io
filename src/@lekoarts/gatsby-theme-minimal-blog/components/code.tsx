467a15e6b82a87078f83c7b99aeeb0f5796dcd6c
SophiaSaiada <sophia.saiada@gmail.com>
2020-01-04 16:32:45 +0200

    Improve code highlight component (add Kotlin badge and link to GitHub).

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/code.tsx
@@ -0,0 +1,162 @@
+/* eslint react/destructuring-assignment: 0 */
+import React from "react";
+import Prism from "prism-react-renderer/prism";
+import kotlinLang from "refractor/lang/kotlin";
+import loadable from "@loadable/component";
+import theme from "prism-react-renderer/themes/nightOwl";
+import useSiteMetadata from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-site-metadata";
+import {
+  HighlightInnerProps,
+  Language
+} from "@lekoarts/gatsby-theme-minimal-blog/src/types";
+import "../styles/prism-atom-dark.css";
+import launchIcon from "../../../images/icons/launch-24px.svg";
+
+kotlinLang(Prism);
+
+type CodeProps = {
+  codeString: string;
+  language: Language;
+  noLineNumbers?: boolean;
+  metastring?: string;
+  [key: string]: any;
+};
+
+const LazyHighlight = loadable(async () => {
+  const Module = await import(`prism-react-renderer`);
+  const Highlight = Module.default;
+  return (props: any) => (
+    <Highlight {...props} Prism={Prism} theme={undefined} />
+  );
+});
+
+const LazyLiveProvider = loadable(async () => {
+  const Module = await import(`react-live`);
+  const { LiveProvider, LiveEditor, LiveError, LivePreview } = Module;
+  return (props: any) => (
+    <LiveProvider {...props}>
+      <LiveEditor data-name="live-editor" />
+      <LiveError />
+      <LivePreview data-name="live-preview" />
+    </LiveProvider>
+  );
+});
+
+function getParams(className = ``) {
+  const [lang = ``, params = ``] = className.split(`:`);
+
+  return [
+    // @ts-ignore
+    lang
+      .split(`language-`)
+      .pop()
+      .split(`{`)
+      .shift()
+  ].concat(
+    // @ts-ignore
+    params.split(`&`).reduce((merged, param) => {
+      const [key, value] = param.split(`=`);
+      // @ts-ignore
+      merged[key] = value;
+      return merged;
+    }, {})
+  );
+}
+
+const RE = /{([\d,-]+)}/;
+
+const calculateLinesToHighlight = (meta: string) => {
+  if (!RE.test(meta)) {
+    return () => false;
+  }
+  const lineNumbers = RE.exec(meta)![1]
+    .split(`,`)
+    .map(v => v.split(`-`).map(x => parseInt(x, 10)));
+  return (index: number) => {
+    const lineNumber = index + 1;
+    const inRange = lineNumbers.some(([start, end]) =>
+      end ? lineNumber >= start && lineNumber <= end : lineNumber === start
+    );
+    return inRange;
+  };
+};
+
+const Code = ({
+  codeString,
+  noLineNumbers = false,
+  className: blockClassName,
+  metastring = ``,
+  ...props
+}: CodeProps) => {
+  const { showLineNumbers } = useSiteMetadata();
+
+  const [language, { title = ``, github = `` }] = getParams(blockClassName);
+  const shouldHighlightLine = calculateLinesToHighlight(metastring);
+
+  const hasLineNumbers =
+    !noLineNumbers && language !== `noLineNumbers` && showLineNumbers;
+
+  if (props[`react-live`]) {
+    return <LazyLiveProvider code={codeString} noInline theme={theme} />;
+  }
+  return (
+    <LazyHighlight code={codeString} language={language} theme={theme}>
+      {({
+        className,
+        style,
+        tokens,
+        getLineProps,
+        getTokenProps
+      }: HighlightInnerProps) => (
+        <React.Fragment>
+          {title && (
+            <div className="code-title">
+              <div>{title}</div>
+            </div>
+          )}
+          <div
+            className={`gatsby-highlight ${(github && "with-github-link") ||
+              ""}`}
+            data-language={language}
+          >
+            <pre
+              className={className}
+              style={style}
+              data-linenumber={hasLineNumbers}
+            >
+              {tokens.map((line, i) => {
+                const lineProps = getLineProps({ line, key: i });
+
+                if (shouldHighlightLine(i)) {
+                  lineProps.className = `${lineProps.className} highlight-line`;
+                }
+
+                return (
+                  <div {...lineProps}>
+                    {hasLineNumbers && (
+                      <span className="line-number-style">{i + 1}</span>
+                    )}
+                    {line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                );
+              })}
+            </pre>
+          </div>
+          {github && (
+            <a
+              className="gatsby-highlight--github--container"
+              href={"https://github.com/" + github}
+            >
+              <img src={launchIcon} />
+              לקוד המלא ב-GitHub
+            </a>
+          )}
+        </React.Fragment>
+      )}
+    </LazyHighlight>
+  );
+};
+
+export default Code;
--- src/@lekoarts/gatsby-theme-minimal-blog/styles/code.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/styles/code.tsx
@@ -1,4 +1,6 @@
 import style from "@lekoarts/gatsby-theme-minimal-blog/src/styles/code";
+import iconLaunch from "../../../images/icons/launch-24px.svg";
+
 export default {
   ...style,
   ".math": {
@@ -17,5 +19,56 @@ export default {
     py: 1,
     mx: 1,
     fontFamily: `'Fira Code', source-code-pro, Menlo, Monaco, Consolas, Courier New, monospace`
+  },
+  ".preview-image-container": {
+    mx: -3,
+    borderRadius: [`0`, `3px`]
+  },
+  ".gatsby-highlight": {
+    ...style[".gatsby-highlight"],
+    direction: "ltr",
+    borderRadius: [`0`, `3px`],
+    mx: -3,
+    boxShadow:
+      "0px 4px 5px -2px rgba(0, 0, 0, 0.1), 0px 7px 10px 1px rgba(0, 0, 0, 0.07), 0px 2px 16px 1px rgba(0, 0, 0, 0.06)",
+    backgroundColor: "#1a1a1a",
+    "&.with-github-link": {
+      borderRadius: [`0`, `3px 3px 0 0`]
+    },
+    pre: {
+      ...style[".gatsby-highlight"]["pre"],
+      fontFamily: `'Fira Code', source-code-pro, Menlo, Monaco, Consolas, Courier New, monospace`
+    },
+    'pre[class~="language-kotlin"]:before': {
+      content: `"Kotlin"`,
+      background: `#d46586`,
+      color: `black`
+    },
+    'pre[class*="language-"]:before': {
+      ...style[".gatsby-highlight"]['pre[class*="language-"]:before'],
+      textTransform: `none`
+    }
+  },
+  ".gatsby-highlight--github--container": {
+    borderRadius: [`0`, `0 0 3px 3px`],
+    mx: -3,
+    boxShadow:
+      "0px 4px 5px -2px rgba(0, 0, 0, 0.1), 0px 7px 10px 1px rgba(0, 0, 0, 0.07), 0px 2px 16px 1px rgba(0, 0, 0, 0.06)",
+    zIndex: 2,
+    position: "relative",
+    background: "#fbfbfb",
+    px: 3,
+    py: 2,
+    fontSize: "0.75rem",
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none",
+    img: {
+      height: "0.75rem",
+      ml: 1
+    },
+    "&:hover": {
+      textDecoration: "underline"
+    }
   }
 };
