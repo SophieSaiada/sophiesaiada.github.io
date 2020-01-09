05ac3574a2057565be882c59800be494566cfac4
Sophia Saiada <sophia.saiada@gmail.com>
2020-01-09 21:10:27 +0200

    Add Disqus

--- src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout";
 import ItemTags from "@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags";
 import SEO from "@lekoarts/gatsby-theme-minimal-blog/src/components/seo";
+import { DiscussionEmbed } from "disqus-react";
 import "katex/dist/katex.min.css";
 
 type PostProps = {
@@ -36,40 +37,48 @@ type PostProps = {
 const px = [`32px`, `16px`, `8px`, `4px`];
 const shadow = px.map(v => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`);
 
-const Post = ({ data: { post } }: PostProps) => (
-  <Layout>
-    <SEO
-      title={post.title}
-      description={post.description ? post.description : post.excerpt}
-      image={post.banner ? post.banner.childImageSharp.resize.src : undefined}
-    />
-    <Heading variant="h2" as="h2">
-      {post.title}
-    </Heading>
-    <p
-      sx={{
-        color: `secondary`,
-        mt: 3,
-        a: { color: `secondary` },
-        fontSize: [1, 1, 2]
-      }}
-    >
-      <time>{post.date}</time>
-      {post.tags && (
-        <React.Fragment>
-          {` — `}
-          <ItemTags tags={post.tags} />
-        </React.Fragment>
-      )}
-    </p>
-    <section
-      sx={{
-        my: 5
-      }}
-    >
-      <MDXRenderer>{post.body}</MDXRenderer>
-    </section>
-  </Layout>
-);
+const Post = ({ data: { post } }: PostProps) => {
+  const disqusConfig = {
+    shortname: "sophiasaiada",
+    config: { identifier: post.slug, title: post.title }
+  };
+
+  return (
+    <Layout sx={{ mb: 4 }}>
+      <SEO
+        title={post.title}
+        description={post.description ? post.description : post.excerpt}
+        image={post.banner ? post.banner.childImageSharp.resize.src : undefined}
+      />
+      <Heading variant="h2" as="h2">
+        {post.title}
+      </Heading>
+      <p
+        sx={{
+          color: `secondary`,
+          mt: 3,
+          a: { color: `secondary` },
+          fontSize: [1, 1, 2]
+        }}
+      >
+        <time>{post.date}</time>
+        {post.tags && (
+          <React.Fragment>
+            {` — `}
+            <ItemTags tags={post.tags} />
+          </React.Fragment>
+        )}
+      </p>
+      <section
+        sx={{
+          my: 5
+        }}
+      >
+        <MDXRenderer>{post.body}</MDXRenderer>
+      </section>
+      <DiscussionEmbed {...disqusConfig} />
+    </Layout>
+  );
+};
 
 export default Post;
