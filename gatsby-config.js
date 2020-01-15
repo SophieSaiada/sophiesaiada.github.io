ce52f52553f0c9cf0c9a060c9526d83daaf09fcf
Sophia Saiada <sophia.saiada@gmail.com>
2020-01-15 11:28:12 +0200

    More SEO things

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,26 +119,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "./src/images/AvatarGithub.png"
-      //   name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-      //   short_name: `minimal-blog`,
-      //   description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-      //   start_url: `/`,
-      //   background_color: `#fff`,
-      //   theme_color: `#6B46C1`,
-      //   display: `standalone`,
-      //   icons: [
-      //     {
-      //       src: `/android-chrome-192x192.png`,
-      //       sizes: `192x192`,
-      //       type: `image/png`
-      //     },
-      //     {
-      //       src: `/android-chrome-512x512.png`,
-      //       sizes: `512x512`,
-      //       type: `image/png`
-      //     }
-      //   ]
+        name: `Sophia Saiada`,
+        short_name: `SophiaSaiada`,
+        start_url: `/`,
+        description: `Sophia Saiada, Full Stack (web & mobile) Developer`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icon: `./src/images/AvatarGithub.png`
       }
     },
     // `gatsby-plugin-offline`,
