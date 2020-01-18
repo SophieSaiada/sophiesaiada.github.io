0280f88f5e2a5a6327491d2ce63cc94372d98769
Sophia Saiada <sophia.saiada@gmail.com>
2020-01-18 15:13:47 +0200

    Add CTA button

--- gatsby-config.js
+++ gatsby-config.js
@@ -132,6 +132,7 @@ module.exports = {
     // `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     // `gatsby-plugin-webpack-bundle-analyser-v2`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
