8a7d99768edea338ecb1eb3e9d696b60440e9a0b
Sophia Saiada <sophia.saiada@gmail.com>
2023-03-30 11:30:52 +0300

    Disable dark mode

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/colormode-toggle.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Fragment } from "react";
+
+const ColorModeToggle = () => <Fragment />;
+export default ColorModeToggle;
--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -3,32 +3,30 @@ import { merge } from "theme-ui";
 
 const secularOne = `'Sora', 'Secular One', sans-serif`;
 const assistant = `'Sora', 'Assistant', sans-serif`;
-const theme = merge(style, {
-  fonts: {
-    heading: secularOne,
-    body: assistant
-  },
-  colors: {
-    primary: "#9c27b0",
-    modes: {
-      dark: {
-        primary: "#9c27b0"
+const theme = merge(
+  { ...style, colors: { ...style.colors, modes: {} } },
+  {
+    fonts: {
+      heading: secularOne,
+      body: assistant
+    },
+    colors: {
+      primary: "#9c27b0"
+    },
+    styles: {
+      li: {
+        pr: 3
+      }
+    },
+    text: {
+      hero: {
+        fontSize: [5, 7, 7],
+        fontFamily: "Sora",
+        fontWeight: 700,
+        letterSpacing: "-0.06em"
       }
     }
-  },
-  styles: {
-    li: {
-      pr: 3
-    }
-  },
-  text: {
-    hero: {
-      fontSize: [5, 7, 7],
-      fontFamily: "Sora",
-      fontWeight: 700,
-      letterSpacing: "-0.06em"
-    }
   }
-});
+);
 
 export default theme;
