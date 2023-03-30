31d5868735075c73cd752802434d521ff4cb6f87
Sophia Saiada <sophia.saiada@gmail.com>
2023-03-30 11:51:37 +0300

    Limit setColorMode calls

--- src/@lekoarts/gatsby-theme-minimal-blog/components/colormode-toggle.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/colormode-toggle.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Fragment } from "react";
 import { useColorMode } from "theme-ui";
 
 const ColorModeToggle = () => {
   const [_colorMode, setColorMode] = useColorMode<"light" | "dark">();
-  setColorMode("light");
+  useEffect(() => {
+    setColorMode("light");
+  }, []);
   return <Fragment />;
 };
 
