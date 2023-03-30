3dfd6948a3369a9db76aa23a01e9e6ecc2cd769e
Sophia Saiada <sophia.saiada@gmail.com>
2023-03-30 11:40:13 +0300

    another approach to enforce light mode

--- src/@lekoarts/gatsby-theme-minimal-blog/components/colormode-toggle.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/colormode-toggle.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { Fragment } from "react";
+import { useColorMode } from "theme-ui";
 
-const ColorModeToggle = () => <Fragment />;
+const ColorModeToggle = () => {
+  const [_colorMode, setColorMode] = useColorMode<"light" | "dark">();
+  setColorMode("light");
+  return <Fragment />;
+};
+
 export default ColorModeToggle;
