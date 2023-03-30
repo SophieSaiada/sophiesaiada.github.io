fc11f34f424d9ad0ffcd5a2a00eef2e735ba5933
Sophia Saiada <sophia.saiada@gmail.com>
2023-03-30 11:05:17 +0300

    Fix CTAs in homepage

--- src/components/cta.jsx
+++ src/components/cta.jsx
@@ -3,12 +3,13 @@ import { Link } from "gatsby";
 import { Box, Text } from "theme-ui";
 import "../style/cta-button.scss";
 
-const CTA = ({ gatsbyLink, text, icon, to }) => (
+const CTA = ({ gatsbyLink, text, icon, to, ...boxProps }) => (
   <Box
     as={gatsbyLink ? Link : "a"}
     {...(gatsbyLink ? { to } : { href: to })}
     className="cta-button"
-    sx={{ fontSize: [1, 2, 2] }}
+    sx={{ fontSize: [1, 2, 2], width: ["100%", "auto", "auto"] }}
+    {...boxProps}
   >
     <img src={icon} alt="" className="cta-button--icon" />
     <Text as="span">{text}</Text>
