1ef3df15e54c91816b05a22d327553e11dd7aa95
Sophia Saiada <sophia.saiada@gmail.com>
2025-06-24 20:43:18 +0300

    fix headlines size on mobile

--- src/components/homepage/section-title.tsx
+++ src/components/homepage/section-title.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from "react";
 
 export const SectionTitle = ({ children }: PropsWithChildren) => (
-  <h2 className="font-caveat text-[3.5rem] text-white-text mt-12 lg:mt-16 mb-8 lg:mb-10 tracking-[0.02em] text-center text-glow-xl font-normal">
+  <h2 className="font-caveat text-[3rem] lg:text-[3.5rem] text-white-text mt-12 lg:mt-16 mb-8 lg:mb-10 tracking-[0.02em] text-center text-glow-xl font-normal">
     {children}
   </h2>
 );
